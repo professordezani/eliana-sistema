1486b796b1a42c3905b0e71c2cc5bf5a3c0adb74
Henrique Dezani <professorhenriquedezani@gmail.com>
2024-07-11 09:25:15 -0300

    add firebase to persist data

--- app.py
+++ app.py
@@ -1,53 +1,94 @@
 from datetime import datetime
 
+import firebase_admin
 import pandas as pd
 import streamlit as st
+from firebase_admin import credentials, firestore
+
+# Initialize Firebase Admin SDK
+if not firebase_admin._apps:
+    cred = credentials.Certificate(
+        "eliana-sistema-firebase-adminsdk-o60ey-34a6a3d5ec.json"
+    )
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 
 # Function to save patient data to a file
 def save_patient_data(name, email, address):
-    # Load existing data or create a new DataFrame
-    try:
-        df = pd.read_csv("patient_data.csv")
-    except FileNotFoundError:
-        df = pd.DataFrame(columns=["Name", "Email", "Address"])
+    # # Load existing data or create a new DataFrame
+    # try:
+    #     df = pd.read_csv("patient_data.csv")
+    # except FileNotFoundError:
+    #     df = pd.DataFrame(columns=["Name", "Email", "Address"])
 
-    # Append new data
-    new_data = pd.DataFrame({"Name": [name], "Email": [email], "Address": [address]})
-    df = pd.concat([df, new_data], ignore_index=True)
+    # # Append new data
+    # new_data = pd.DataFrame({"Name": [name], "Email": [email], "Address": [address]})
+    # df = pd.concat([df, new_data], ignore_index=True)
 
-    # Save to CSV
-    df.to_csv("patient_data.csv", index=False)
+    # # Save to CSV
+    # df.to_csv("patient_data.csv", index=False)
+    db.collection("patients").add({"name": name, "email": email, "address": address})
+    # st.sidebar.success('Patient Registered Successfully!')
 
 
 # Function to save exam data to a file
 def save_exam_data(name, glucose, cholesterol, operator):
     # Load existing data or create a new DataFrame
-    try:
-        df = pd.read_csv("exam_data.csv")
-    except FileNotFoundError:
-        df = pd.DataFrame(
-            columns=["Name", "Date", "Glucose", "Cholesterol", "Operator"]
-        )
+    # try:
+    #     df = pd.read_csv("exam_data.csv")
+    # except FileNotFoundError:
+    #     df = pd.DataFrame(
+    #         columns=["Name", "Date", "Glucose", "Cholesterol", "Operator"]
+    #     )
 
-    # Append new data
-    new_data = pd.DataFrame(
+    # # Append new data
+    # new_data = pd.DataFrame(
+    #     {
+    #         "Name": [name],
+    #         "Date": [datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
+    #         "Glucose": [glucose],
+    #         "Cholesterol": [cholesterol],
+    #         "Operator": [operator],
+    #     }
+    # )
+    # df = pd.concat([df, new_data], ignore_index=True)
+
+    # # Save to CSV
+    # df.to_csv("exam_data.csv", index=False)
+    db.collection("exams").add(
         {
-            "Name": [name],
-            "Date": [datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
-            "Glucose": [glucose],
-            "Cholesterol": [cholesterol],
-            "Operator": [operator],
+            "name": name,
+            "glucose": glucose,
+            "cholesterol": cholesterol,
+            "operator": operator,
+            "date": firestore.SERVER_TIMESTAMP,  # Server timestamp for the current date
         }
     )
-    df = pd.concat([df, new_data], ignore_index=True)
 
-    # Save to CSV
-    df.to_csv("exam_data.csv", index=False)
+
+def get_patients():
+    patients_ref = db.collection("patients")
+    patients = patients_ref.get()
+    patient_data = []
+    for patient in patients:
+        patient_data.append(patient.to_dict())
+    return patient_data
+
+
+def get_exams(patient_name):
+    exams_ref = db.collection("exams").where("name", "==", patient_name)
+    exams = exams_ref.get()
+    exam_data = []
+    for exam in exams:
+        exam_data.append(exam.to_dict())
+    return exam_data
 
 
 # Main function to run the Streamlit app
 def main():
+
     st.title("Registro de Exames")
 
     # Sidebar for registration
@@ -61,8 +102,9 @@ def main():
 
     # Main section for exam records
     st.header("Patient Exam Records")
-    patients = pd.read_csv("patient_data.csv")
-    patient_names = patients["Name"].tolist()
+    # patients = pd.read_csv("patient_data.csv")
+    # patient_names = patients["Name"].tolist()
+    patient_names = [patient["name"] for patient in get_patients()]
     selected_patient = st.selectbox("Select a Patient", patient_names)
 
     # Input fields for exam records
@@ -76,11 +118,31 @@ def main():
         # Clear input fields after saving
 
     # Display exam records
-    exam_data = pd.read_csv("exam_data.csv")
-    selected_patient_data = exam_data[exam_data["Name"] == selected_patient]
+    # exam_data = pd.read_csv("exam_data.csv")
+    # selected_patient_data = exam_data[exam_data["Name"] == selected_patient]
+    selected_patient_data = next(
+        (patient for patient in get_patients() if patient["name"] == selected_patient),
+        None,
+    )
+    # st.write(selected_patient_data)
 
-    st.write(selected_patient_data)
+    # Display exams for the selected patient
+    exams = get_exams(selected_patient_data["name"])
+    if exams:
+        st.subheader("Exams")
+        exam_df = pd.DataFrame(exams)
+        st.dataframe(exam_df)
+        # for exam in exams:
+        #     st.write(f"Date: {exam['date']}")
+        #     st.write(f"Glucose: {exam['glucose']}")
+        #     st.write(f"Cholesterol: {exam['cholesterol']}")
+        #     st.write(f"Operator: {exam['operator']}")
+        #     st.write(
+        #         f"Comments: {exam.get('comments', '')}"
+        #     )  # Adjust as per your Firestore data model
+        #     st.write("---")
 
 
 if __name__ == "__main__":
+
     main()
